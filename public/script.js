86b64d289ca8fa0970b2414f775a442fbc3aa560
akshit <akshitsadana@gmail.com>
2020-08-02 00:11:41 +0530

    video apt

--- public/script.js
+++ public/script.js
@@ -1,7 +1,34 @@
+// setting up socket connection and peerJS server
 const socket = io('/');
+const peer = new Peer(undefined, {
+    host: '/', 
+    port: '3001'
+});
 
-socket.emit('join-room', ROOM_ID, 10);
+// video functions
+const videoGrid = document.getElementById('video-grid');
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then((stream) => {
+    streamVideo(myVideo, stream);
+})
+
+peer.on('open', (userId) => {
+    socket.emit('join-room', ROOM_ID, userId);
+});
 
 socket.on('user-connected', (userId) => {
-    console.log('New user joined the room' + userId);
+    console.log(`${userId} joined the room`);
 });
+
+function streamVideo(video, stream) {
+    video.scrObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    });
+    videoGrid.append(video);
+};
